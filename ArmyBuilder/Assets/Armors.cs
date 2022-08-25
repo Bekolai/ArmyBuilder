dfa74fd023ed9b409201dbd9661727928affa92b
Bekolai <berkayaydin1010@gmail.com>
2022-08-25 11:56:47 +0300

    Imported LOWPOLY - Universal Character (Modular). Created armor holders, imported FREE Slavic Medieval Environment Town Interior and exterior,Mobile depth water shader

---
+++ ArmyBuilder/Assets/Armors.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armors : MonoBehaviour
+{
+    [SerializeField] List<GameObject> armor1, armor2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.Z))
+        {
+            foreach(GameObject armor in armor1)
+            {
+                armor.SetActive(true);
+                foreach (GameObject otherarmor in armor2)
+                { 
+                otherarmor.SetActive(false);
+                }
+
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            foreach (GameObject armor in armor2)
+            {
+                armor.SetActive(true);
+                foreach (GameObject otherarmor in armor1)
+                {
+                    otherarmor.SetActive(false);
+                }
+
+            }
+        }
+    }
+}
+
